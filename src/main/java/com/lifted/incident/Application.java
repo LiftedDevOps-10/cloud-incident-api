5580bd752a920a1bdf7c85e868ca074e829138fc
LiftedDevOps-10 <liftedbymercy24@gmail.com>
2026-09-18 13:29:19 +0100

    Test Lambda handler locally

---
+++ src/main/java/com/lifted/incident/Application.java
@@ -0,0 +1,14 @@
+package com.lifted.incident;
+
+public class Application {
+
+    public static void main(String[] args) {
+
+        System.out.println("======================================");
+        System.out.println("      CLOUD INCIDENT API");
+        System.out.println("======================================");
+        System.out.println("Java Version: 21");
+        System.out.println("Application Status: RUNNING");
+        System.out.println("======================================");
+    }
+}
